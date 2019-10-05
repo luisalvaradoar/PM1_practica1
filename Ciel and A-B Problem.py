df7d78e899fe6d8f053e3a0fa7c3735dc997d2eb
luisalvaradoar <43264748+luisalvaradoar@users.noreply.github.com>
2019-10-05 11:17:51 -0600

    Update Ciel and A-B Problem.py

--- Ciel and A-B Problem.py
+++ Ciel and A-B Problem.py
@@ -1,7 +1,10 @@
 try:
     numeros = input().split(" ")
     N = str(int(numeros[0])-int(numeros[1]))
-    N_nuevo = N[0:len(N) - 1] + str((int(N[-1]) + 1) % 10)
+    if int(N[-1]<9):
+        N_nuevo = int(numeros[0])-int(numeros[1]) + 1
+    else:
+        N_nuevo = int(numeros[0])-int(numeros[1]) - 1
     print(N_nuevo)
 except EOFError:
     pass
